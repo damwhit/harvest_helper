21c41890e9667595024f4b04959093d1ccca04a2
David Whitaker <damwhit@gmail.com>
2016-04-22 18:16:27 -0600

    user sees plant show page test is written

---
+++ spec/integration/user_can_view_specific_plants_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "User sees a specific plant" do
+  context "logged in user, when searching", :js => true do
+    before do
+      @user = stub_omniauth
+      @plant = create(:plant)
+    end
+
+    it "should see the plant that was searched for" do
+
+      visit '/plants'
+
+      fill_in 'search', with: "#{@plant.name}"
+
+      find('input#search').native.send_keys(:return)
+
+      expect(current_path).to eq("/plants/#{@plant.id}")
+      expect(page).to have_content("#{@plant.name}")
+
+    end
+
+    it "should see render index if plant doesn't exist." do
+
+      visit '/plants'
+
+      fill_in 'search', with: "poop"
+
+      find('input#search').native.send_keys(:return)
+
+      expect(current_path).to eq("/plants")
+      expect(page).to have_content("Sorry, couldn't find that plant. Search again.
+")
+    end
+  end
+end
