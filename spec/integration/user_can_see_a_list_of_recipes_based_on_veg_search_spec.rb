3d97790f67d860d56151bc742afd5accf7f9a407
David Whitaker <damwhit@gmail.com>
2016-04-24 13:21:00 -0600

    show path for a given recipe is tested

--- spec/integration/user_can_see_a_list_of_recipes_based_on_veg_search_spec.rb
+++ spec/integration/user_can_see_a_list_of_recipes_based_on_veg_search_spec.rb
@@ -6,17 +6,23 @@ RSpec.feature "User sees a list of recipes for a given vegetable" do
       @user = stub_omniauth
       @plant = create(:plant, name: "carrot")
     end
+    VCR.use_cassette("recipe_search") do
       it "returns a successful search", js: true do
         visit "/plants"
         fill_in "recipe_search", with: "carrot"
         find("input#recipe_search").native.send_keys(:return)
 
-        VCR.use_cassette("recipe_search") do
 
 
         expect(current_path).to eq('/recipes')
-        expect(page).to have_content("Carrot")
 
+        link = "Deborah Madison's Ivory Carrot Soup with a Fine Dice of Orange Carrots"
+
+        click_on(link)
+
+        path = "/recipes/Deborah%20Madison's%20Ivory%20Carrot%20Soup%20with%20a%20Fine%20Dice%20of%20Orange%20Carrots"
+
+        expect(current_path).to eq(path)
       end
     end
   end
