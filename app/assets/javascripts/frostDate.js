f1c42bf2648385be424af1333a5fbeff413dd9c4
David Whitaker <damwhit@gmail.com>
2016-04-25 19:13:46 -0600

    zip search now uses ajax calls instead of rails

--- app/assets/javascripts/frostDate.js
+++ app/assets/javascripts/frostDate.js
@@ -1,16 +1,31 @@
-var input = $('#zip')
+var zipInput = $('#zip')
 var tag = $('#tag')
+var id = tag.attr("data-plant")
 
-input.keypress(function(e){
+function add(htmltag, frostdate) {
+
+var block = [
+'<p class="plant-text">spring date: ' + frostdate.spring + '<br>fall date: ' + frostdate.fall +
+'</p><p class="plant-text">*both fall and spring frost dates are based on threshold of 32 degrees and 50% probability</p>'
+];
+
+return htmltag.prepend(block);
+}
+
+zipInput.keypress(function(e){
   var key = e.which;
   if(key == 13){
     console.log("pressed enter")
+    console.log(tag.attr("data-plant"))
+    $('#frost-search').empty()
+    e.preventDefault()
     $.ajax({
-      url: '/plants/' + tag.attr("data-plant") + '?zip=' + input.val(),
+      url: '/plants/' + id + '?utf8=%E2%9C%93&zip=' + zipInput.val(),
       type: 'GET',
       dataType: "json",
-    }).success(function(frostdates) {
-      console.log(frostdates)
+    }).success(function(frostdate) {
+      console.log(frostdate)
+      add($('#frost-search'), frostdate)
     })
   }
 });
