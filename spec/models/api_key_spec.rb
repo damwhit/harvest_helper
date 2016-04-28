58261c36e070183aa416a1681517f18cfacd640a
David Whitaker <damwhit@gmail.com>
2016-04-27 22:43:21 -0600

    model relations now tested

--- spec/models/api_key_spec.rb
+++ spec/models/api_key_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe ApiKey, type: :model do
+  it { should belong_to :user }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:oauth_token) }
+  it { should have_many :plants }
+  it { should have_many :api_keys }
 end
+#
