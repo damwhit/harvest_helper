e55a83b4bdec00852246c1c7116232570e2094c0
David Whitaker <damwhit@gmail.com>
2016-06-22 22:27:32 -0600

    write test for recipe index based on plant

---
+++ spec/requests/api/v1/recipes_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "GET /api/v1/recipes" do
+  it "returns a list of all recipes for a given plant" do
+    include ApiHelpers
+    plant1 = create(:plant)
+    create(:plant, name: "test plant numero dos")
+    api_key = create(:api_key)
+
+    get "/api/v1/recipes?plant=#{plant1.name}&api_key=#{api_key.api_key}"
+
+    expect(response.status).to eq(200)
+    plant1_json = json_body[0]
+    expect(plant1_json).to eq({:id=>plant1.id,
+      :name=>plant1.name,
+      :description=>"oh so guud",
+      :optimal_sun=>"full",
+      :optimal_soil=>"loamy",
+      :planting_considerations=>"lots of things to consider",
+      :when_to_plant=>"spring",
+      :growing_from_seed=>"do it",
+      :transplanting=>"ok",
+      :spacing=>"10-12 inches apart",
+      :watering=>"lots",
+      :feeding=> plant1.feeding,
+      :other_care=>"weed and stoof",
+      :diseases=>"yikes",
+      :pests=>"bugs fa dayz",
+      :harvesting=>"use ya hands",
+      :storage_use=>"fridge fa days",
+      :image => "https://s3.amazonaws.com/harvest-helper/plants/images/000/000/001/medium/01_tomato.jpg",
+    })
+  end
+end
