2efb20ab49d89fa8dfa8269a86fffeed6844c880
David Whitaker <damwhit@gmail.com>
2018-05-21 17:56:57 -0600

    Switch before filter to before action

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class ApiController < ApplicationController
       protect_from_forgery with: :null_session
-      before_filter :restrict_access
+      before_action :restrict_access
       respond_to :json
 
       private
