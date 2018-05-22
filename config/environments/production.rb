413b6aa4579c08033d7435cbf0adef269e4e5a77
David Whitaker <damwhit@gmail.com>
2018-05-22 09:12:16 -0600

    Add in defaults for action mailer in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.action_mailer.default_url_options = { :host => 'harvesthelper.heroku.com' }
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
