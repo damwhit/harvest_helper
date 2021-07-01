5a783257294c923a7e48dd1645325d6c40cd2fce
David Whitaker <damwhit@gmail.com>
2021-07-01 12:27:56 -0400

    Get storybook to use tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,14 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     'storybook-addon-designs',
-    'storybook-zeplin/register'
+    'storybook-zeplin/register',
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ]
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,4 @@
+import "../app/frontend/stylesheets/index.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
